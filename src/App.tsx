74457c7983decc19e1b1f2bb3c1e75039e444018
ProtoSg <diegoalbertosalazar177@gmail.com>
2024-11-08 12:16:29 -0500

    feat: add routs admin and login

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom"
 import { Admin, Home, Layaout, Login, PassengerDetails, SearchResult, SeatSelection } from "./pages"
+import { Asiento, Bus, Conductor, Descuento, ProgramacionViaje, Ruta, Terminal } from "./pages/admin/pages"
 
 function App() {
   return (
@@ -11,7 +12,15 @@ function App() {
         <Route path='/passenger-details' element={<PassengerDetails />} />
       </Route>
       <Route path='/login' element={<Login />} />
-      <Route path='/admin' element={<Admin />} />
+      <Route path='/admin' element={<Admin />} >
+        <Route path="programacion-viaje" element={<ProgramacionViaje />} />
+        <Route path="ruta" element={<Ruta />} />
+        <Route path="bus" element={<Bus />} />
+        <Route path="terminal" element={<Terminal />} />
+        <Route path="descuento" element={<Descuento />} />
+        <Route path="conductor" element={<Conductor />} />
+        <Route path="asiento" element={<Asiento />} />
+      </Route>
     </Routes >
   )
 }
