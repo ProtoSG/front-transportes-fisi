2058c06468d4f190279fa1e8c79d75528dae01b6
ProtoSg <diegoalbertosalazar177@gmail.com>
2024-11-08 12:16:21 -0500

    feat: add login components

--- src/pages/login/components/FormLogin.tsx
+++ src/pages/login/components/FormLogin.tsx
@@ -3,14 +3,26 @@ import { LoginData, schemaLogin } from "../model/formLogin.model"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { InputFieldLight, NewButton } from "../../../components"
 import { LockIcon, UserOutlineIcon } from "../../../icons"
+import { useNavigate } from "react-router-dom"
 
 export function FormLogin() {
-  const { control, handleSubmit, setValue, formState: { errors } } = useForm<LoginData>({
-    resolver: zodResolver(schemaLogin)
+  const { control, handleSubmit, formState: { errors } } = useForm<LoginData>({
+    resolver: zodResolver(schemaLogin),
+    defaultValues: {
+      username: "",
+      password: "",
+    }
   })
 
+  const navigete = useNavigate()
+
+  const onSubmit = (data: LoginData) => {
+    console.log(data)
+    navigete("/admin")
+  }
+
   return (
-    <form className="flex flex-col gap-8 w-72 mx-auto">
+    <form className="flex flex-col gap-8 w-72 mx-auto" onSubmit={handleSubmit(onSubmit)}>
       <InputFieldLight
         type="text"
         control={control}
--- src/pages/login/components/TitleLogin.tsx
+++ src/pages/login/components/TitleLogin.tsx
@@ -4,7 +4,7 @@ export function TitleLogin() {
   return (
     <div className='absolute top-[30%] left-1/2 -translate-x-1/2 -translate-y-1/2  flex gap-7 justify-center items-center'>
       <img src={logo} alt="logo" className="w-14" />
-      <h1 className='text-6xl text-primary-200 font-bold text-nowrap'>Transportes FISI</h1>
+      <h1 className='text-6xl text-white font-bold text-nowrap'>Transportes FISI</h1>
       <img src={logo} alt="logo" className="w-14" />
     </div>
   )
