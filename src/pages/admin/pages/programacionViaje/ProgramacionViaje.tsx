dc66211bfbed579e06068e5a249d8b930fdeec60
ProtoSG <diegoalbertosalazar177@gmail.com>
2024-11-08 21:30:55 -0500

    feat: update ProgramacionViaje screan

--- src/pages/admin/pages/programacionViaje/ProgramacionViaje.tsx
+++ src/pages/admin/pages/programacionViaje/ProgramacionViaje.tsx
@@ -1,5 +1,7 @@
+import { DialogHTMLAttributes } from "react";
 import { NewButton } from "../../../../components";
-import { Table } from "../../components";
+import { DialogAddEdit, Table } from "../../components";
+import { FormProgramacionViaje } from "./components";
 
 const createColumns = (keys: string[]) =>
   keys.map((key) => ({
@@ -89,19 +91,35 @@ export function ProgramacionViaje() {
     }
   ]
 
+  const handleOpenDialog = () => {
+    const dialog = document.getElementById('dialog-programacion-viaje') as HTMLDialogElement
+    if (dialog) dialog.showModal()
+  }
 
   return (
-    <section className="bg-inherit text-white flex flex-col gap-10">
-      <div className="flex justify-between py-6">
-        <h3 className="text-4xl font-bold text-nowrap">Programacion Viaje</h3>
-        <NewButton className="w-52">Crear Nuevo</NewButton>
-      </div>
-      <div>
-        <Table
-          columns={columns}
-          data={data}
-        />
-      </div>
-    </section>
+    <>
+      <section className="bg-inherit w-[80%] text-white flex flex-col gap-10">
+        <div className="flex justify-between py-6">
+          <h3 className="text-4xl font-bold text-nowrap">Programacion Viaje</h3>
+          <NewButton
+            className="max-w-44"
+            onClick={handleOpenDialog}
+          >
+            Crear Nuevo
+          </NewButton>
+        </div>
+        <div className="overflow-x-scroll w-full">
+          <Table
+            columns={columns}
+            data={data}
+          />
+        </div>
+      </section>
+      <DialogAddEdit
+        id="dialog-programacion-viaje"
+      >
+        <FormProgramacionViaje />
+      </DialogAddEdit>
+    </>
   )
 }
