03e7d2cc18138f505ebf06274e4fcbf54ca00a21
ProtoSg <diegoalbertosalazar177@gmail.com>
2024-11-13 17:53:09 -0500

    feat: add PerfilAdmin route

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,23 @@ import Home from "./pages/home/Home"
 import { PassengerDetails, SearchResult, SeatSelection, User } from "./pages/user"
 import Login from "./pages/login/Login"
 import { Admin } from "./pages/admin/Admin"
-import { Asiento, Bus, Conductor, Descuento, ProgramacionViaje, Ruta, Terminal } from "./pages/admin/pages"
+import { AdminProfile, Asiento, Bus, Conductor, Descuento, ProgramacionViaje, Ruta, Terminal } from "./pages/admin/pages"
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+
+      <Route path='/login' element={<Login />} />
+
       <Route path="" element={<User />}>
         <Route path='/search-results' element={<SearchResult />} />
         <Route path='/seat-selection' element={<SeatSelection />} />
         <Route path='/passenger-details' element={<PassengerDetails />} />
       </Route>
-      <Route path='/login' element={<Login />} />
+
       <Route path='/admin' element={<Admin />} >
+        <Route path="" element={<AdminProfile />} />
         <Route path="programacion-viaje" element={<ProgramacionViaje />} />
         <Route path="ruta" element={<Ruta />} />
         <Route path="bus" element={<Bus />} />
