aa7b9a01ae438b8045c96fccacdbb70e25e3ce04
ProtoSg <diegoalbertosalazar177@gmail.com>
2024-11-13 14:55:07 -0500

    feat: update Bus container

--- src/pages/admin/pages/bus/Bus.tsx
+++ src/pages/admin/pages/bus/Bus.tsx
@@ -1,5 +1,76 @@
+import { DialogAddEdit, Header, Section, Table } from "../../components";
+import { createColumns } from "../../services/createColumns";
+import { FormBus } from "./components";
+
 export function Bus() {
+
+  const columns = createColumns([
+    "id",
+    "asientos",
+    "placa",
+    "marca",
+    "niveles",
+    "id_tipo_servicio_bus"
+  ])
+
+  const data = [
+    {
+      id: "1",
+      asientos: "2",
+      placa: "123456",
+      marca: "Mercedes",
+      niveles: "2",
+      id_tipo_servicio_bus: "1"
+    },
+    {
+      id: "2",
+      asientos: "2",
+      placa: "123456",
+      marca: "Mercedes",
+      niveles: "2",
+      id_tipo_servicio_bus: "1"
+    },
+    {
+      id: "3",
+      asientos: "2",
+      placa: "123456",
+      marca: "Mercedes",
+      niveles: "2",
+      id_tipo_servicio_bus: "1"
+    },
+    {
+      id: "4",
+      asientos: "2",
+      placa: "123456",
+      marca: "Mercedes",
+      niveles: "2",
+      id_tipo_servicio_bus: "1"
+    },
+  ]
+
+  const handleOpenDialog = () => {
+    const dialog = document.getElementById('dialog-bus') as HTMLDialogElement
+    if (dialog) dialog.showModal()
+  }
+
   return (
-    <div>Bus</div>
+    <>
+      <Section>
+        <Header
+          title="Bus"
+          onClick={handleOpenDialog}
+        />
+        <Table
+          columns={columns}
+          data={data}
+        />
+      </Section>
+      <DialogAddEdit
+        id="dialog-bus"
+        title="Nuevo Bus"
+      >
+        <FormBus />
+      </DialogAddEdit>
+    </>
   )
 }
