c21a8c3dcce969348622f330ac5b0608fe4c3ff2
deyvigo <deyvi.gomez@unmsm.edu.pe>
2024-11-14 16:22:28 -0500

    add: payment methods delete implementation

--- src/pages/client/hooks/api/useClientPaymentMethods.ts
+++ src/pages/client/hooks/api/useClientPaymentMethods.ts
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react'
-import { getClientPaymentMethods } from '../../services/clientPaymentMethods'
+import { deleteClientPaymentMethod, getClientPaymentMethods } from '../../services/clientPaymentMethods'
 import { loadFromLocalStorage } from '../../../../services/localStorageActions'
 
 export interface CreditCardProps {
-  id_metodo_pago: number,
+  id_metodo_pago: string,
   nombre: string,
   numero_tarjeta: string,
 }
@@ -17,7 +17,7 @@ export const useGetClientPaymentMethods = () => {
     setLoading(true)
     setError(null)
 
-    const token = loadFromLocalStorage<string>("jwt_token", "")
+    const token = loadFromLocalStorage<string>("jwt_token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTczMTYxMzE1MiwianRpIjoiMzExMzgzNjctMDlmZi00OTQ3LWJiNTEtZDIyMjkzNGZiZWEyIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6eyJpZF9jbGllbnRlIjo0LCJ1c2VybmFtZSI6ImNsaWVudGU0IiwiZnVsbG5hbWUiOiJKdWRpdGggSXpxdWllcmRvIFB1Z2EifSwibmJmIjoxNzMxNjEzMTUyLCJjc3JmIjoiMzE5ODcxMWEtOTcxOS00MmZmLTkxNzMtNjhmZjZmZjQwZTcwIiwiZXhwIjoxNzMyMjE3OTUyfQ.2dZOs7N2k7NwStz4Q8gLmYITIQ23xd-gXRFBhNjG2rc")
 
     try {
       const res = await getClientPaymentMethods(token)
@@ -38,6 +38,38 @@ export const useGetClientPaymentMethods = () => {
 
   return {
     creditCards,
+    loading,
+    error,
+  }
+}
+
+export const useDeleteClientPaymentMethod = () => {
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<any>(null)
+  const [idDeleted, setIdDeleted] = useState('')
+
+  const deleteCreditCard = async (id: string) => {
+    setLoading(true)
+    setError(null)
+
+    const token = loadFromLocalStorage<string>("jwt_token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTczMTYxMzE1MiwianRpIjoiMzExMzgzNjctMDlmZi00OTQ3LWJiNTEtZDIyMjkzNGZiZWEyIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6eyJpZF9jbGllbnRlIjo0LCJ1c2VybmFtZSI6ImNsaWVudGU0IiwiZnVsbG5hbWUiOiJKdWRpdGggSXpxdWllcmRvIFB1Z2EifSwibmJmIjoxNzMxNjEzMTUyLCJjc3JmIjoiMzE5ODcxMWEtOTcxOS00MmZmLTkxNzMtNjhmZjZmZjQwZTcwIiwiZXhwIjoxNzMyMjE3OTUyfQ.2dZOs7N2k7NwStz4Q8gLmYITIQ23xd-gXRFBhNjG2rc")
+
+    try {
+      const res = await deleteClientPaymentMethod(token, id)
+      if (res.error) {
+        throw new Error(res.error)
+      }
+      setIdDeleted(res.id_metodo_pago)
+    } catch (err) {
+      setError(err)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return {
+    deleteCreditCard,
+    idDeleted,
     loading,
     error,
   }
--- src/pages/client/services/clientPaymentMethods.ts
+++ src/pages/client/services/clientPaymentMethods.ts
@@ -27,3 +27,31 @@ export const getClientPaymentMethods = async (token: string) => {
     return { error: err }
   }
 }
+
+export const deleteClientPaymentMethod = async (token: string, id: string) => {
+  const apiUrl = `${API_URL}/client/payment-method/${id}`
+  try {
+    const response = await fetch(apiUrl, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      },
+    })
+
+    // if not send token the api return 422
+    if (response.status === 422) {
+      throw new Error('No se ha podido autenticar')
+    }
+
+    if (!response.ok) {
+      const resJson = await response.json()
+      throw new Error(resJson.error)
+    }
+
+    const resJson = await response.json()
+    return resJson
+  } catch (err) {
+    return { error: err }
+  }
+}
