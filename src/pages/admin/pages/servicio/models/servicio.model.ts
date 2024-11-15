ad2e651f71bda9bcba133916595619de67deaecc
ProtoSg <diegoalbertosalazar177@gmail.com>
2024-11-15 12:24:13 -0500

    feat: add formServicio and servicio servicio/models

---
+++ src/pages/admin/pages/servicio/models/servicio.model.ts
@@ -0,0 +1,13 @@
+export type Servicio = {
+  id: number,
+  servicio: string
+}
+
+export type ServicioBack = {
+  id_tipo_servicio_bus: number;
+  servicio: string;
+}
+
+export type ServicioCreate = {
+  servicio: string
+}
