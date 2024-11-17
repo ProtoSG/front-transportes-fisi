041799cd1094fbc40b97975c6eef5f528d615bcc
deyvigo <deyvi.gomez@unmsm.edu.pe>
2024-11-17 13:50:38 -0500

    feat: pdf ticket

---
+++ src/pages/client/hooks/api/useTicket.ts
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import { loadFromLocalStorage } from '../../../../services/localStorageActions'
+import { generateTicket } from '../../services/clientTickets'
+
+export const useTicket = () => {
+  const [ticketName, setTicketName] = useState<string>('')
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState<any>(null)
+
+  const fetchInfo = async (idPasaje: string) => {
+    setLoading(true)
+    setError(null)
+
+    const token = loadFromLocalStorage<string>("jwt_token", "")
+
+    try {
+      const res = await generateTicket(token, idPasaje)
+      if (res.error) {
+        throw new Error(res.error)
+      }
+      setTicketName(res.ticket_name)
+    } catch (err) {
+      setError(err)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return {
+    fetchInfo,
+    ticketName,
+    loading,
+    error
+  }
+}
--- src/pages/client/pages/boleto/component/PrintButton.tsx
+++ src/pages/client/pages/boleto/component/PrintButton.tsx
@@ -1,11 +1,32 @@
-import { PdfIcon } from "../../../../../icons"
+import { toast, Toaster } from 'sonner'
+import { PdfIcon } from '../../../../../icons'
+import { useTicket } from '../../../hooks/api/useTicket'
+import { useEffect } from 'react'
+const API_URL = import.meta.env.VITE_BACKEND_URL
 
 export const PrintButton = ({ id_pasaje }: { id_pasaje: string }) => {
+  const { fetchInfo, error, ticketName } = useTicket()
+
+  useEffect(() => {
+    if (error) toast.error(error.message)
+  }, [error])
+
+  useEffect(() => {
+    if (ticketName) {
+      toast.success(`Boleto generado con éxito`)
+      const urlTicekt = `${API_URL}/ticket/pdf/${ticketName}`
+      window.open(urlTicekt, '_blank')
+    }
+  }, [ticketName])
+
   return (
-    <button className="hover:bg-primary-500 hover:scale-105 hover:text-white transition-colors rounded-sm p-2 text-white duration-300 ease-in-out"
-      onClick={() => { console.log(id_pasaje) }}
-    >
-      <PdfIcon className="text-primary-200" />
-    </button>
+    <>
+      <button className="hover:bg-primary-500 hover:scale-105 hover:text-white transition-colors rounded-sm p-2 text-white duration-300 ease-in-out"
+        onClick={() => { fetchInfo(id_pasaje) }}
+      >
+        <PdfIcon className="text-primary-200" />
+      </button>
+      <Toaster richColors />
+    </>
   )
 }
