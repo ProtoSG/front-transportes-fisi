061e9652326be420d9460742d96c2eaf8d0bee7d
BrayanAlq <brayan.alquizar@unmsm.edu.pe>
2024-11-24 16:25:20 -0500

    add: createChofer hook

--- src/pages/admin/pages/conductor/components/FormConductor.tsx
+++ src/pages/admin/pages/conductor/components/FormConductor.tsx
@@ -4,30 +4,57 @@ import { conductorData, schemaConductor } from "../models/formConductor.model";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { InputFieldLight } from "../../../../../components";
 import { formField } from "../../../models/formField.model";
+import { useCreateChofer } from "../../../../../hooks/useCreateChofer";
 
 export function FormConductor() {
-  const { control, handleSubmit, formState: { errors } } = useForm<conductorData>({
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<conductorData>({
     resolver: zodResolver(schemaConductor),
     defaultValues: {
       nombre: "",
       apellido_pat: "",
       apellido_mat: "",
       dni: "",
-      sexo: ""
-    }
-  })
+      sexo: "",
+    },
+  });
+
+  const { createChofer, error } = useCreateChofer();
 
   const formFields: formField<conductorData>[] = [
     { name: "nombre", type: "text", placeholder: "Nombre", label: "Nombre" },
-    { name: "apellido_pat", type: "text", placeholder: "Apellido Pat", label: "Apellido Pat" },
-    { name: "apellido_mat", type: "text", placeholder: "Apellido Mat", label: "Apellido Mat" },
+    {
+      name: "apellido_pat",
+      type: "text",
+      placeholder: "Apellido Pat",
+      label: "Apellido Pat",
+    },
+    {
+      name: "apellido_mat",
+      type: "text",
+      placeholder: "Apellido Mat",
+      label: "Apellido Mat",
+    },
     { name: "dni", type: "text", placeholder: "Dni", label: "Dni" },
-    { name: "sexo", type: "text", placeholder: "Sexo", label: "Sexo" }
-  ]
+    { name: "sexo", type: "text", placeholder: "Sexo", label: "Sexo" },
+  ];
 
-  const onSubmit = (data: conductorData) => {
-    console.log({ data })
-  }
+  const onSubmit = async (data: conductorData) => {
+    try {
+      await createChofer(data);
+      if (error) {
+        alert("Error al crear el conductor. Por favor, inténtalo de nuevo."); 
+        return;
+      }
+      alert("¡Conductor creado con éxito!");
+    } catch (err) {
+      alert("Error al crear el conductor. Por favor, inténtalo de nuevo.");
+      console.error(err);
+    }
+  };
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
@@ -43,5 +70,5 @@ export function FormConductor() {
         />
       ))}
     </Form>
-  )
+  );
 }
