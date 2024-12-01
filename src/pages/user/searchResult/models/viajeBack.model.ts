13f4724aa7fca80c4dc9e212685f3a531f0d041a
ProtoSg <diegoalbertosalazar177@gmail.com>
2024-12-01 00:03:31 -0500

    feat: adapt viaje and viajeBack model

--- src/pages/user/searchResult/models/viajeBack.model.ts
+++ src/pages/user/searchResult/models/viajeBack.model.ts
@@ -1,23 +1,13 @@
 export interface ViajeBack {
-  id_viaje: number,
-  embarque: {
-    hora: string,
-    id_embarque: number,
-    ubicacion: string
-  },
-  desembarque: {
-    hora: string,
-    id_embarque: number,
-    ubicacion: string
-  },
-  servicio: {
-    id_servicio: number,
-    nombre: string
-  },
-  bus: {
-    id_bus: number,
-    piso: number
-  },
-  asientos_disponibles: number,
-  precio: number
+  asientos_disponibles: number
+  destino: string
+  distancia: number
+  duracion: string
+  fecha_salida: string
+  hora_llegada: string
+  hora_salida: string
+  id_viaje_programado: number
+  origen: string
+  precio_min: number
+  servicio: string
 }
