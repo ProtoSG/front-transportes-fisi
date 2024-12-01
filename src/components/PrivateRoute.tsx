6564ea99bbdcf95a37b48d9039908abb963a2edc
ProtoSG <diegoalbertosalazar177@gmail.com>
2024-12-01 14:42:02 -0500

    feat: add private routes for admin and client

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,12 +1,22 @@
 import { Navigate } from "react-router-dom"
 import { loadFromLocalStorage } from "../services/localStorageActions"
 import { ReactNode } from "react"
+import { User } from "../pages/login/model/user.model"
 
 interface Props {
   children: ReactNode
 }
 
-export const PrivateRoute = ({ children }: Props) => {
+export const PrivateRouteAdmin = ({ children }: Props) => {
   const token = loadFromLocalStorage<string>("jwt_token", "")
-  return token ? children : <Navigate to="/login/admin" replace />
+  const user = loadFromLocalStorage<User>("user", {} as User)
+
+  return token && user.role === "admin" ? children : <Navigate to={`/login/admin}`} replace />
+}
+
+export const PrivateRouteClient = ({ children }: Props) => {
+  const token = loadFromLocalStorage<string>("jwt_token", "")
+  const user = loadFromLocalStorage<User>("user", {} as User)
+
+  return token && user.role === "client" ? children : <Navigate to={`/login/admin}`} replace />
 }
